c62b6fac55f2dcc34191c1ee5ab4664d5bd0b5dc
adburke <adburke@fullsail.edu>
2013-03-18 14:04:09 -0500

    Updated error handling needed for android

--- AVFdroid/assets/www/scripts/controllers/main.js
+++ AVFdroid/assets/www/scripts/controllers/main.js
@@ -1,21 +1,26 @@
 'use strict';
 
-angular.module('angularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Testacular'
-    ];
-  });
+// angular.module('angularApp')
+//   .controller('MainCtrl', function ($scope) {
+//     $scope.awesomeThings = [
+//       'HTML5 Boilerplate',
+//       'AngularJS',
+//       'Testacular'
+//     ];
+//   });
 
 function twitCtrl($scope, $http) {
   $scope.input = '';
   $scope.search = function () {
-    $http.jsonp('https://search.twitter.com/search.json?q=%40' + $scope.input + '&callback=JSON_CALLBACK')
+    $http.jsonp('http://search.twitter.com/search.json?q=%40' + $scope.input + '&rpp=5&callback=JSON_CALLBACK')
       .success(function(data) {
         $scope.tweets = data.results;
         console.log(data);
+      })
+      .error(function(data, status) {
+          $scope.data = data || "Request failed";
+          $scope.status = status;
+          console.log(status);
       });
   };
 }
@@ -27,6 +32,11 @@ function ytubeCtrl($scope, $http) {
       .success(function(data) {
         $scope.videos = data.feed.entry;
         console.log(data);
+      })
+      .error(function(data, status) {
+          $scope.data = data || "Request failed";
+          $scope.status = status;
+          console.log(status);
       });
   };
 }
