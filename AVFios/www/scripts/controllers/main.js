0effe99cbc433a862a98454fa2278d3d3a01b042
adburke <adburke@fullsail.edu>
2013-03-21 01:45:36 -0500

    Native work

--- AVFios/www/scripts/controllers/main.js
+++ AVFios/www/scripts/controllers/main.js
@@ -11,7 +11,7 @@ angular.module('angularApp')
 
 
 function twitCtrl($scope, $http) {
-  $scope.input = '';
+  // $scope.input = '';
   $scope.search = function () {
     $http.jsonp('http://search.twitter.com/search.json?q=%40' + $scope.input + '&rpp=5&callback=JSON_CALLBACK')
       .success(function(data) {
@@ -27,7 +27,7 @@ function twitCtrl($scope, $http) {
 }
 
 function ytubeCtrl($scope, $http) {
-  $scope.input = '';
+  // $scope.input = '';
   $scope.search = function () {
     $http.jsonp('https://gdata.youtube.com/feeds/api/videos?category=' + $scope.input + '&alt=json&max-results=6&v=2&callback=JSON_CALLBACK')
       .success(function(data) {
@@ -43,7 +43,31 @@ function ytubeCtrl($scope, $http) {
 }
 
 function nativeCtrl($scope) {
+  $scope.image = "./img/cordova.png";
   $scope.getImage = function (imageURI) {
     $scope.image = imageURI;
   };
+
+  $scope.getGeo = function (position) {
+    $scope.latitude = position.coords.latitude;
+    $scope.longitude = position.coords.longitude;
+    $scope.altitude = position.coords.altitude;
+    $scope.accuracy = position.coords.accuracy;
+    $scope.altitudeAcc = position.coords.altitudeAccuracy;
+    $scope.heading = position.coords.heading;
+    $scope.speed = position.coords.speed;
+    $scope.timestamp = position.timestamp;
+  };
+
+  $scope.getVideo = function (videoArray) {
+    $scope.video = videoArray[0].fullPath;
+  };
+
+  $scope.getAccel = function (acceleration) {
+    $scope.accelX = acceleration.x;
+    $scope.accelY = acceleration.y;
+    $scope.accelZ = acceleration.z;
+    $scope.accelTimestamp = acceleration.timestamp;
+  };
+
 };
--- AVFios/www/scripts/directives/directives.js
+++ AVFios/www/scripts/directives/directives.js
@@ -11,8 +11,69 @@ app.directive('camera', function() {
                    scope.getImage(imageURI);
                });
             }, function (err) {
-               ctrl.$setValidity('error', false);
-            }, { quality: 50, destinationType: Camera.DestinationType.FILE_URI, saveToPhotoAlbum: true } );
+                  ctrl.$setValidity('error', false);
+               },
+               { quality: 50, destinationType: Camera.DestinationType.FILE_URI, saveToPhotoAlbum: true }
+            );
+         });
+      }
+   };
+});
+
+app.directive('geolocation', function() {
+   return {
+      restrict: 'A',
+      link: function(scope, element, attrs, ctrl) {
+         element.bind('click', function() {
+            // console.log('Fired');
+            navigator.geolocation.getCurrentPosition(function (position) {
+               scope.$apply(function() {
+                   scope.getGeo(position);
+               });
+            }, function (err) {
+                  ctrl.$setValidity('error', false);
+               }
+            );
+         });
+      }
+   };
+});
+
+app.directive('takevideo', function() {
+   return {
+      restrict: 'A',
+      link: function(scope, element, attrs, ctrl) {
+         element.bind('click', function() {
+            // console.log('Fired');
+            navigator.device.capture.captureVideo(function (videoArray) {
+               scope.$apply(function() {
+                   scope.getVideo(videoArray);
+               });
+            }, function (err) {
+                  ctrl.$setValidity('error', false);
+                  console.log(err);
+               }
+            );
+         });
+      }
+   };
+});
+
+app.directive('accel', function() {
+   return {
+      restrict: 'A',
+      link: function(scope, element, attrs, ctrl) {
+         element.bind('click', function() {
+            // console.log('Fired');
+            navigator.accelerometer.watchAcceleration(function (acceleration) {
+               scope.$apply(function() {
+                   scope.getAccel(acceleration);
+               });
+            }, function (err) {
+                  ctrl.$setValidity('error', false);
+                  console.log(err);
+               }
+            );
          });
       }
    };
